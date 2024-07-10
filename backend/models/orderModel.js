3fd2df86a43d1918e4e9eedad421683a35a96d23
Allfin <maulinaro@yahoo.com>
2024-07-10 15:38:46 +0700

    first-commit

---
+++ backend/models/orderModel.js
@@ -0,0 +1,80 @@
+import mongoose from 'mongoose';
+
+const orderSchema = mongoose.Schema(
+  {
+    snap_token: {
+      type: String,
+    },
+    snap_redirect_url: {
+      type: String,
+    },
+    payment_method: {
+      type: String,
+    },
+    snap_token: {
+      type: String,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    orderItems: [
+      {
+        name: { type: String, required: true },
+        qty: { type: Number, required: true },
+        image: { type: String, required: true },
+        price: { type: Number, required: true },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          required: true,
+          ref: 'Product',
+        },
+      },
+    ],
+    shippingAddress: {
+      address: { type: String, required: true },
+      city: { type: String, required: true },
+      courierNote: { type: String, required: true },
+      numberPhone: { type: Number, required: true },
+    },
+    itemsPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model('Order', orderSchema);
+
+export default Order;
---
+++ frontend/src/screens/ShippingScreen.jsx
@@ -0,0 +1,99 @@
+import { useState } from 'react';
+import { Form, Button } from 'react-bootstrap';
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import FormContainer from '../components/FormContainer';
+import CheckoutSteps from '../components/CheckoutSteps';
+import { saveShippingAddress } from '../slices/cartSlice';
+
+const ShippingScreen = () => {
+  // catatan yang dihapus
+  // ---- Order ----
+  // 1. postal kode => kurir note
+  // 2. numberPhone => number phone
+  // 3. payment method
+  // 4. tax price
+
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const [address, setAddress] = useState(shippingAddress.address || '');
+  const [recipientName, setRecipientName] = useState(
+    shippingAddress.city || ''
+  );
+  const [curierNote, setCurierNote] = useState(
+    shippingAddress.curierNote || ''
+  );
+  const [numberPhone, setNumberPhone] = useState(
+    shippingAddress.numberPhone || ''
+  );
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(
+      saveShippingAddress({ address, recipientName, curierNote, numberPhone })
+    );
+    navigate('/payment');
+  };
+
+  return (
+    <FormContainer>
+      <CheckoutSteps step1 step2 />
+      <h1>Shipping</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className='my-2' controlId='address'>
+          <Form.Label>Alamat Lengkap</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='masukan alamat lengkap'
+            value={address}
+            required
+            onChange={(e) => setAddress(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className='my-2' controlId='city'>
+          <Form.Label>Nama Penerima</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Masukan nama penerima'
+            value={recipientName}
+            required
+            onChange={(e) => setRecipientName(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className='my-2' controlId='curierNote'>
+          <Form.Label>Catatan Untuk Kuri (Opsional)</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Catatan untuk kurir'
+            value={curierNote}
+            required
+            onChange={(e) => setCurierNote(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Form.Group className='my-2' controlId='numberPhone'>
+          <Form.Label>Nomor Hp</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Enter numberPhone'
+            value={numberPhone}
+            required
+            onChange={(e) => setNumberPhone(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+
+        <Button type='submit' variant='success'>
+          Lanjutkan
+        </Button>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default ShippingScreen;
